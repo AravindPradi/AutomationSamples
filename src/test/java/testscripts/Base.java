322e416723ab7c77eb84ed25b5a0b4d92af068cf
Aravind Pradeep <aravind.pradeep.0802@gmail.com>
2024-11-05 20:20:47 +0530

    first commit

---
+++ src/test/java/testscripts/Base.java
@@ -0,0 +1,36 @@
+package testscripts;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base{
+	public WebDriver driver;
+	public void initializeBrowser() {
+		driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		
+	}
+	
+	public void driverQuit() {
+	driver.quit();
+	}
+	
+	public static void main(String[] args) {
+		Base base = new Base();
+		base.initializeBrowser();
+		base.driverQuit();
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
